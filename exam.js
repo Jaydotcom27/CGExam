911dbdbff9b8bd334fa4fc4e76b82c3b283d936b
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-01-25 15:42:03 -0400

    reverseArray functions done

--- exam.js
+++ exam.js
@@ -118,31 +118,46 @@
 
 // console.log(alphabetizeString("Lorem ipsum"))
 
-function findMostPopularItem(array){
+// function findMostPopularItem(array){
 
-    array.sort()
-    let mostPopularItem = array[0]
-    let counter = 1
-    let maxReached = 1
-    let arrayLength = array.length;
+//     array.sort()
+//     let mostPopularItem = array[0]
+//     let counter = 1
+//     let maxReached = 1
+//     let arrayLength = array.length;
 
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] == array[i-1]){
-            counter++
-        }else{
-            if( counter > maxReached){
-                maxReached = counter
-                mostPopularItem = array[i - 1]
-            }
-            counter = 1
-        }
+//     for (let i = 1; i < arrayLength; i++) {
+//         if (array[i] == array[i-1]){
+//             counter++
+//         }else{
+//             if( counter > maxReached){
+//                 maxReached = counter
+//                 mostPopularItem = array[i - 1]
+//             }
+//             counter = 1
+//         }
         
+//     }
+//     if(counter > maxReached){
+//         maxReached = counter
+//         mostPopularItem = array[arrayLength - 1]
+//     }
+//     return mostPopularItem
+// }
+
+// console.log(findMostPopularItem([1,5,5,2,3,4,5,5,6,7,7,7]))
+
+function reverseArray(array){
+    let temporalArray = []
+    for (let i = array.length - 1; i >= 0; i--) {
+        temporalArray.push(array[i])
     }
-    if(counter > maxReached){
-        maxReached = counter
-        mostPopularItem = array[arrayLength - 1]
-    }
-    return mostPopularItem
+    array = temporalArray
+    return array
 }
 
-console.log(findMostPopularItem([1,2,3,4,5,5,6,7,7,7]))
+function reverseArrayInPlace(array){
+    return array.reverse()
+}
+
+console.log(reverseArrayInPlace([1,2,3]))
