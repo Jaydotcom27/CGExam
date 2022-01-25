2fa2d7c92dfd880fe0a34d445217cc4cf3221f53
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-01-25 13:54:31 -0400

    return Bs done

--- exam.js
+++ exam.js
@@ -7,21 +7,31 @@
 //             console.log(`2, 3, 5 and 7 are not multiples of ${number}`)
 //         }
 // }
-
 // firstProblem(210)
 
-function FizzBuzz(){
-    for (let i = 1; i <= 100; i++) {
-        if (i%15 === 0){
-            console.log("FizzBuzz")
-        }else if (i%5 === 0){
-            console.log("Buzz")
-        } else if (i%3 === 0){
-            console.log("Fizz");
-        } else {
-            console.log(i)
-        }
-    }
-}
+// function FizzBuzz(){
+//     for (let i = 1; i <= 100; i++) {
+//         if (i%15 === 0){
+//             console.log("FizzBuzz")
+//         }else if (i%5 === 0){
+//             console.log("Buzz")
+//         } else if (i%3 === 0){
+//             console.log("Fizz");
+//         } else {
+//             console.log(i)
+//         }
+//     }
+// }
+// FizzBuzz()
 
-FizzBuzz()
+// function returnBs(word){
+//     let stringLength = word.length
+//     let count = 0
+//     for (let i = 0; i < stringLength; i++) {
+//         if (word.charAt(i) == "B"){
+//             count++
+//         }  
+//     }
+//     console.log(count)
+// }
+// returnBs("BBcb");
