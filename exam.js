1c2d24a10d5d15f57dceb820b082b6f96d1fd8af
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-01-25 13:41:02 -0400

    firstProblem done

---
+++ exam.js
@@ -0,0 +1,13 @@
+function firstProblem(number){
+    let indicator = false;
+        if ((number%2 == 0) && (number%3 == 0) && (number%5 == 0) && (number%7 == 0)){
+            indicator = true
+            console.log(`2, 3, 5 and 7 are multiples of ${number}`)
+        } else{
+            console.log(`2, 3, 5 and 7 are not multiples of ${number}`)
+        }
+}
+
+firstProblem(210)
+
+
