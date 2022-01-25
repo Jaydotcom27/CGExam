e9a4ebb88e64d65846a2630e4c145e820f42e39d
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-01-25 14:26:31 -0400

    convertToOrdinal done

--- exam.js
+++ exam.js
@@ -44,3 +44,32 @@
 //     }
 // }
 // console.log(getFactorial(5))
+
+function convertToOrdinal(number){
+    let lastDigit = number.toString().slice(-1)
+    if((number === 12  || number === 13  )){
+        console.log(`${number}th`)
+    } 
+    else if((lastDigit === '1' && number != 11)){
+        console.log(`${number}st`)
+    } else if ((lastDigit === '2' && number != 12)){
+        console.log(`${number}nd`)
+    } else if ((lastDigit === '3' && number != 13)){
+        console.log(`${number}rd`)
+    } 
+    else {
+        console.log(`${number}th`)
+    }
+}
+
+convertToOrdinal(1)
+convertToOrdinal(2)
+convertToOrdinal(3)
+convertToOrdinal(11)
+convertToOrdinal(12)
+convertToOrdinal(13)
+convertToOrdinal(21)
+convertToOrdinal(22)
+convertToOrdinal(23)
+convertToOrdinal(123)
+
