dc42058b7a9578802177071a25a0aaffac6ba007
Jeremy Martínez <jeremy-alejandro_28@hotmail.com>
2022-01-25 14:06:02 -0400

    getFactorial done

--- exam.js
+++ exam.js
@@ -35,3 +35,12 @@
 //     console.log(count)
 // }
 // returnBs("BBcb");
+
+// function getFactorial(number){
+//     if ( number === 0){
+//         return 1;
+//     } else {
+//         return (number * getFactorial(number - 1)) 
+//     }
+// }
+// console.log(getFactorial(5))
